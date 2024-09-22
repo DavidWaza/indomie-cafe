370548899a37f177593ed9e789ec221bf7a40102
david waza <davidyega69@gmail.com>
2024-09-22 19:15:40 +0100

    fix: hero padding fix

--- src/components/AppDownload.tsx
+++ src/components/AppDownload.tsx
@@ -29,10 +29,13 @@ const AppDownload = () => {
               className="leading-[30px] py-3 lg:hidden block !text-[22px]"
             >
               download Our App Today! <br />
-              Treat Your Taste Buds To 
-              Heaven
+              Treat Your Taste Buds To Heaven
             </Typography>
-            <Typography variant="primary" size="sm" className="lg:w-1/2 lg:pt-5">
+            <Typography
+              variant="primary"
+              size="sm"
+              className="lg:w-1/2 lg:pt-5"
+            >
               Lorem ipsum dolor sit amet, consectetur adipisic ingelit, sed do
               eiusmod tempor incididunt utlabo re et dolore magna aliqua.
             </Typography>
@@ -61,10 +64,16 @@ const AppDownload = () => {
           </div>
         </div>
         <div className="doodle-bg">
-
+          <Image
+            src={`https://res.cloudinary.com/dgbl43ljm/image/upload/v1727028669/Bike_and_Phone_qy8ec1.png`}
+            alt="banner image"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-1/2 h-auto m-auto object-center lg:pt-0 object-contain"
+          />
         </div>
       </div>
-
     </div>
   );
 };
